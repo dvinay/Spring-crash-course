6ef18339e7c6aff4cc20cd01461a7f6f2e664ffc
vinay duvvada <vinay.chowdary518@gmail.com>
2017-12-16 19:45:24 -0800

    created product entity

---
+++ springorm/src/main/java/com/fuppino/spring/springorm/product/entity/Product.java
@@ -0,0 +1,59 @@
+package com.fuppino.spring.springorm.product.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+	@Id
+	@Column(name="id")
+	private int id;
+	@Column(name="name")
+	private String name;
+	@Column(name="description")
+	private String desc;
+	@Column(name="price")
+	private double price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", desc=" + desc
+				+ ", price=" + price + "]";
+	}
+
+}
