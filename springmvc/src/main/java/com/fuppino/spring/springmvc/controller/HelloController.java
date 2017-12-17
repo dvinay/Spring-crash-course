e0b585a985d29b2aabe39f8a4d69045b7cd35c63
vinay duvvada <vinay.chowdary518@gmail.com>
2017-12-16 23:15:04 -0800

    package name has changed

--- springmvc/src/main/java/com/fuppino/spring/springmvc/controller/HelloController.java
+++ springmvc/src/main/java/com/fuppino/spring/springmvc/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.fuppino.spring.springmvc;
+package com.fuppino.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
