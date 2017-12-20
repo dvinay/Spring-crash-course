55762a62ebd53689fe87e347a3c10191969e13c4
vinay duvvada <vinay.chowdary518@gmail.com>
2017-12-20 02:09:56 -0800

    created rest services

--- springbootjpa/src/main/java/com/fuppino/springboot/controllers/ProductController.java
+++ springbootjpa/src/main/java/com/fuppino/springboot/controllers/ProductController.java
@@ -1,7 +1,12 @@
 package com.fuppino.springboot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +25,21 @@ public class ProductController {
 		Iterable<Product> products = productRepository.findAll();
 		return products;
 	}
+	
+	@GetMapping("/{id}")
+	public Product getProducts(@PathVariable("id") Long id) {
+		Product product = productRepository.findOne(id);
+		return product;
+	}
+	
+	@PostMapping
+	public Product createProduct(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+	
+	@PutMapping
+	public Product updateProduct(Product product) {
+		return productRepository.save(product);
+	}
+
 }
